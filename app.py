c4b41f0d383f8bbecf6521c3e923489b80e69b56
ashishkumargoyal <drashishjaipur@gmail.com>
2025-01-04 17:37:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title("ARSH Bot")
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-4.0-3.5-turbo"
+    st.session_state["openai_model"] = "gpt-4.0"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
